2b8111a6c1e92475cbf5333fff6a5d8c24a766b2
poojarynikitha <126642704+poojarynikitha@users.noreply.github.com>
2023-08-04 20:56:18 +0530

    Update generator.sv

--- generator.sv
+++ generator.sv
@@ -1,7 +1,28 @@
-class and_transaction;
+`include "defines.sv"
+class and_generator;
 //PROPERTIES
-//INPUTS declare as rand variables
-  rand bit [1:0] a;
-  rand bit [1:0] b;
-  //Output
-  bit y;
+  //and transaction class handle 
+  and_transaction  blueprint;
+  //Mailbox for generator to driver connection
+  mailbox #(and_transaction)mbx_gd;
+  //METHODS
+  //Explicitly overriding the constructor to make mailbox connection 
+  //from generator to driver
+  function new(mailbox #(and_transaction)mbx_gd);
+    this.mbx_gd=mbx_gd;
+    blueprint=new();
+  endfunction
+   //Task to generate the random stimuli
+  task start();
+    for(int i=0;i<`no_of_trans;i++)
+      begin
+      //Randomizing the inputs
+        blueprint.randomize();
+      //Putting the randomized inputs to mailbox    
+        mbx_gd.put(blueprint.copy());  
+        $display("GENERATOR Randomized transaction a=%d,b=%d,",
+                  blueprint.a,blueprint.b,$time);
+      end
+  endtask
+endclass
+
